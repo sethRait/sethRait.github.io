4681ca3ddd3ad615b83fe302ea6e8a5c3257cd32
Seth Rait <sethrait95@gmail.com>
2015-07-02 16:12:13 -0400

    inline is not in line

--- js/jquery.js
+++ js/jquery.js
@@ -1,8 +1,8 @@
 /**
  * Created by srait on 7/2/2015.
  */
-$( document ).ready(function() {
+/*$( document ).ready(function() {
     $( "p" ).click(function( event ) {
         alert( "Thanks for visiting!" );
     });
-});
+});*/

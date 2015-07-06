b0da3fb64a4e616aed024d99314351d65b66f3c4
Seth Rait <sethrait95@gmail.com>
2015-07-06 13:03:32 -0400

    beginning work on forms

--- js/jquery.js
+++ js/jquery.js
@@ -1,8 +1,22 @@
 /**
  * Created by srait on 7/2/2015.
  */
-/*$( document ).ready(function() {
-    $( "p" ).click(function( event ) {
-        alert( "Thanks for visiting!" );
+$( document ).ready(function() {
+    $("form").submit(function(){
+        appendText();
+        $.ajax({
+            type: "POST",
+            url: "bin/process.php",
+            data: dataString,
+            success: function() {
+                $('#contact_form').html("<div id='message'></div>");
+                $('#message').html("<h2>Contact Form Submitted!</h2>")
+                    .append("<p>We will be in touch soon.</p>")
+            }
+        });
+        return false;
     });
-});*/
+    function appendText() {
+        $("p").append("Some appended text.");
+    }
+});

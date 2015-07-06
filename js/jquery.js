9fcb1b3adbabf8b64eed0d60ba63ddaf965f2e41
Seth Rait <sethrait95@gmail.com>
2015-07-06 14:43:14 -0400

    things work a bit

--- js/jquery.js
+++ js/jquery.js
@@ -2,21 +2,8 @@
  * Created by srait on 7/2/2015.
  */
 $( document ).ready(function() {
-    $("form").submit(function(){
-        appendText();
-        $.ajax({
-            type: "POST",
-            url: "bin/process.php",
-            data: dataString,
-            success: function() {
-                $('#contact_form').html("<div id='message'></div>");
-                $('#message').html("<h2>Contact Form Submitted!</h2>")
-                    .append("<p>We will be in touch soon.</p>")
-            }
-        });
-        return false;
+    $("form").submit(function (e) {
+        e.preventDefault();
+        $(".form_wrapper").append("<p>APPENDED</p>");
     });
-    function appendText() {
-        $("p").append("Some appended text.");
-    }
 });

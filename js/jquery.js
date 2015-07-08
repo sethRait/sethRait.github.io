5fba2aada7603ccb076d6b285f5615fea06f2a53
Seth Rait <sethrait95@gmail.com>
2015-07-08 16:43:54 -0400

    refactored and alligned text

--- js/jquery.js
+++ js/jquery.js
@@ -2,9 +2,16 @@
  * Created by srait on 7/2/2015.
  */
 $( document ).ready(function() {
-    $("form").submit(function (e) {
-        e.preventDefault();
-        $("form").replaceWith("<p>TEXT</p>");
-        $(".form_wrapper").append("<form class='terminal-text'><p class='terminal-text'>guest@sethrait:~ $</p> <input name='terminal' size='80' placeholder='echo Currently under construction, please come back later'> <input type='hidden' value='Search'> </form>");
+    $(".terminal-window").on("submit", "form", function (e) { //when form is submitted
+        submitCommand(e);
     });
 });
+
+//triggered when user submits a command to the console
+function submitCommand(e){
+    e.preventDefault();
+    var usrCommand=$('.form_input').val();
+    var location="guest@sethrait:~ $ ";
+    $(".form_wrapper").replaceWith("<p class='new-terminal-text' style='margin-left: -12px'>"+location+usrCommand+"</p>"+"<br>");
+    $(".terminal-window").append("<div class='form_wrapper'><form class='terminal-text'><p class='terminal-text'>guest@sethrait:~ $</p> <input class='form_input' name='terminal' size='80' placeholder='echo Currently under construction, please come back later'> <input type='hidden' value='Search'></form></div>");
+}

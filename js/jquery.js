91b6764668a2e2ba8169ce1dc41929e67a2d4f7c
Seth Rait <sethrait95@gmail.com>
2015-07-09 16:39:58 -0400

    adding commands

--- js/jquery.js
+++ js/jquery.js
@@ -1,6 +1,10 @@
 /**
  * Created by srait on 7/2/2015.
  */
+
+var commands=['ls', 'cd', 'cat', 'grep', 'man', 'about', 'contact', 'resume'];
+var location="guest@sethrait:~ $ ";
+
 $( document ).ready(function() {
     $(".terminal-window").on("submit", "form", function (e) { //when form is submitted
         submitCommand(e);
@@ -11,7 +15,23 @@ $( document ).ready(function() {
 function submitCommand(e){
     e.preventDefault();
     var usrCommand=$('.form_input').val();
-    var location="guest@sethrait:~ $ ";
+    processCommand(usrCommand);
     $(".form_wrapper").replaceWith("<p class='new-terminal-text' style='margin-left: -12px'>"+location+usrCommand+"</p>"+"<br>");
     $(".terminal-window").append("<div class='form_wrapper'><form class='terminal-text'><p class='terminal-text'>guest@sethrait:~ $</p> <input class='form_input' name='terminal' size='80' placeholder='echo Currently under construction, please come back later'> <input type='hidden' value='Search'></form></div>");
+}
+
+//processes the contents of the user submitted command
+function processCommand(usrCommand){
+    for(var i=0;i<commands.length;i++){
+        if(usrCommand===commands[i]){
+            execute(commands[i]);
+        }else{
+            //WRITE THIS
+        }
+    }
+}
+
+//executes the user's command
+function execute(command){  //WRITE THIS FUNCTION
+
 }

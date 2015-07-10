553772b7494714c1aee8062657c9e4da043a295a
Seth Rait <sethrait95@gmail.com>
2015-07-10 11:36:14 -0400

    added sections

--- js/jquery.js
+++ js/jquery.js
@@ -3,7 +3,6 @@
  */
 
 var commands=['ls', 'cd', 'cat', 'grep', 'man', 'about', 'contact', 'resume'];
-var location="guest@sethrait:~ $ ";
 
 $( document ).ready(function() {
     $(".terminal-window").on("submit", "form", function (e) { //when form is submitted
@@ -16,6 +15,7 @@ function submitCommand(e){
     e.preventDefault();
     var usrCommand=$('.form_input').val();
     processCommand(usrCommand);
+    var location="guest@sethrait:~ $ ";
     $(".form_wrapper").replaceWith("<p class='new-terminal-text' style='margin-left: -12px'>"+location+usrCommand+"</p>"+"<br>");
     $(".terminal-window").append("<div class='form_wrapper'><form class='terminal-text'><p class='terminal-text'>guest@sethrait:~ $</p> <input class='form_input' name='terminal' size='80' placeholder='echo Currently under construction, please come back later'> <input type='hidden' value='Search'></form></div>");
 }

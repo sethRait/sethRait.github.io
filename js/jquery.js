f3ab35d08a12e3bdf637de1dc0f5f5dbbb82238c
Seth Rait <sethrait95@gmail.com>
2015-09-06 21:12:02 -0400

    Resume

--- js/jquery.js
+++ js/jquery.js
@@ -1,5 +1,5 @@
 /**
- * Created by srait on 7/2/2015.
+ * Created by Seth Rait on 7/2/2015 in an attempt to recreate the wheel.
  */
 
 var commands=['ls', 'cd', 'grep', 'help', 'about', 'contact', 'links', 'resume'];
@@ -19,7 +19,7 @@ $( document ).ready(function() {
 //gets the width of the terminal window to be used for the textarea
 function getWidth(){
     var textAreaWidth=$("form").width()-$("#p_term").width();
-    $("textarea").css("width", textAreaWidth-5);
+    $("textarea").css("width", textAreaWidth-5); //5 as buffer
 }
 
 //gets the height of the window and applies it to the first two divs
@@ -35,7 +35,7 @@ function submitCommand(e){
     var usrCommand=$('.form_input').val();
     processCommand(usrCommand);
     $(".form_wrapper").replaceWith("<p class='new-terminal-text'>"+loc+usrCommand+"</p>"+"<br>");
-    $("#terminal-window").append("<div class='form_wrapper'><form class='terminal-text'><p id='p_term' class='terminal-text'>guest@sethrait:~ $ </p><textarea autofocus class='form_input' name='terminal' placeholder='Currently under construction, please come back later'></textarea></form></div>");
+    $("#terminal-window").append("<div class='form_wrapper'><form class='terminal-text'><p id='p_term' class='terminal-text'>guest@sethrait:~ $ </p><textarea class='form_input' name='terminal' placeholder='Currently under construction, please come back later'></textarea></form></div>");
     getWidth();
     $(".form_input").keydown(function(){
         if(event.keyCode==13){
@@ -58,23 +58,26 @@ function processCommand(usrCommand){
 //executes the user's command
 function execute(command){
     switch(command) {
-        case commands[3]:
-            $("#terminal-window").append("<p id='p_term' class='new-terminal-text'>The following commands are acceptable for use: <br>ls, cd, cat, grep, about, contact, links, resume </p><br>");
+        case commands[3]:   //help
+            $("#terminal-window").append("<p id='p_term' class='new-terminal-text'>The following commands are acceptable for use: <br>ls, cd, cat, grep, about, contact, links, resume</p>");
             break;
-        case commands[4]:
+        case commands[4]:   //about
             $('html, body').animate({
                 scrollTop: $("#About").offset().top
             }, 1000);
             break;
-        case commands[5]:
+        case commands[5]:   //contact
             $('html, body').animate({
                 scrollTop: $("#Contact").offset().top
             }, 1000);
             break;
-        case commands[6]:
+        case commands[6]:   //links
             $('html, body').animate({
                 scrollTop: $("#Links").offset().top
             }, 1000);
+            break;
+        case commands[7]:   //resume
+            window.open('sources/Resume.pdf');
             break;
     }
 }
